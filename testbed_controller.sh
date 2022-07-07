bcc6f76576de9f5c19b821435bb8d4e673865cf9
Marc Michalke <marc@michalke-it.de>
2022-07-07 11:20:49 +0200

    Added image tags and testbed_controller.sh

---
+++ testbed_controller.sh
@@ -0,0 +1,9 @@
+TIMESTAMP=$(date +%m%d-%H%M)
+TOOLKITPATH="$PWD/deployment_toolkit"
+
+source test_scheduler/test_scheduler.sh
+source deployment_toolkit/prerequisites.sh
+
+bootstrap
+deploy_and_test vm
+deploy_and_test pm
